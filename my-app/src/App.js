10fb31f6acc1c0d6360a2271ba2249b86f1d27c8
julioguzman19 <48781716+julioguzman19@users.noreply.github.com>
2019-11-03 14:50:28 -0700

    Home page and Nav initialized success

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import About from "./pages/About";
+import Catering from "./pages/Catering";
+import Contact from "./pages/Contact";
+import Home from "./pages/Home";
+import Media from "./pages/Media";
+import Menu from "./pages/Menu";
+import Nav from "./components/Nav";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Nav />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/home" component={Home} />
+          {/* <Route exact path="/books/:id" component={Detail} />
+            <Route component={NoMatch} />  */}
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ my-app/src/components/Nav/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Nav() {
+    return (
+        <div>
+            <nav>
+                <h1>nav bro</h1>
+            </nav>
+
+        </div>
+    );
+}
+
+export default Nav;
