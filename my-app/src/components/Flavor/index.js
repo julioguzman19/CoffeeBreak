b8713e187c89220e411b12619f1060f332a560b3
julioguzman19 <48781716+julioguzman19@users.noreply.github.com>
2019-11-12 21:56:19 -0700

    crepe image added via import on index.js file

--- my-app/src/components/Flavor/index.js
+++ my-app/src/components/Flavor/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./style.css";
+import crepe from '../Flavor/crepe.jpg';
 
 function Flavor() {
     return (
         <div class="flavorComponentBackground">
-            <div id="flavorImage">Flavor of the Day!</div>
+            <div id="flavorImage"><img src={crepe} class="center" alt="crepe" width="200px" height="200px" /><h1>Flavor of the Day!</h1></div>
             <div id="flavorName">
                 Strawberry Crepe
                 <br></br>Price:$1
